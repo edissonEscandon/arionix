b904e22383f7af0c9a27d0387f562481d0a34e93
edissonEscandon <84981265+edissonEscandon@users.noreply.github.com>
2025-02-23 07:33:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php include'includes/addinfo.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Altec</title>
+	<?php include'includes/links.php'; ?>
+</head>
+<body>
+	<?php include'includes/header.php'; ?>
+
+	<?php include'includes/top_leters.php'; ?>
+
+	<?php include'includes/icons_logros.php'; ?>
+
+	<?php include'includes/card_row.php'; ?>
+
+
+
+	<div class="agentes-law">
+
+		<div class="row-center">
+			<div>
+				<h2>Nuestros Abogados destacados</h2>
+				<p>Altec es tu plataforma de confianza para conectar con abogados expertos y acceder a recursos legales de calidad. Nuestro objetivo es brindarte asesoría clara, precisa y personalizada para que tomes decisiones informadas sobre tus asuntos legales.</p>
+			</div>
+		</div>
+
+		<div class="general-cards">
+			
+			<div class="subsections-cards">
+
+				<div class="section-image">
+					<img src="images/google.jpg" alt="">
+					<span><i class="fa-solid fa-certificate"></i></span>
+				</div>
+
+				<div class="section-name">
+					<span><i class="fa-solid fa-location-dot"></i> Cuenca-Ecuador</span>
+					<h4>Leonor Escandon</h4>
+				</div>
+
+				<div class="socialmedias">
+					<a href=""><i class="fa-brands fa-facebook"></i></a>
+					<a href=""><i class="fa-brands fa-square-instagram"></i></a>
+					<a href=""><i class="fa-brands fa-x-twitter"></i></a>
+					<a href=""><i class="fa-brands fa-linkedin"></i></a>
+				</div>
+
+				<div class="btn-comunications">
+					<button class="btn-click"><i class="fa-solid fa-envelope"></i> Mnesaje</button>
+					<button><i class="fa-solid fa-square-phone"></i></button>
+				</div>
+				
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+
+	<?php include'includes/scripts.php'; ?>
+</body>
+</html>
